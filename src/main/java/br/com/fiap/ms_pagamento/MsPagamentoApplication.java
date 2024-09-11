c696c278307f024e5bdbd7994b469744ae5591a8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 09:57:56 -0300

    versao com server

--- src/main/java/br/com/fiap/ms_pagamento/MsPagamentoApplication.java
+++ src/main/java/br/com/fiap/ms_pagamento/MsPagamentoApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.ms_pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPagamentoApplication {
 
 	public static void main(String[] args) {
